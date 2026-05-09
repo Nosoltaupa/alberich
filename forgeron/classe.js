28046e0d4b4d0b6a9c96b135204d340bf1b69b30
Nosoltaupa <leonard.lefrere@proton.me>
2026-05-09 11:25:36 +0200

    Update classe.js
    update forgeron's spells

--- forgeron/classe.js
+++ forgeron/classe.js
@@ -14,8 +14,10 @@ const CLASS_DATA = {
 
   // Compétence ultime (se débloque au niveau 6)
   ultime: {
-    nom: "Graveur de runes",
-    desc: "Le forgeron peut graver des runes mystiques sur la Norlithe afin de leur octroyer des capacités particulières.<br>Thurisaz : rune de la sauvagerie, à graver sur une arme, qui augmente ses DEG de 1.<br>Ansuz : rune de la connaissance, à graver sur le crâne, qui augmente l'ES de 1.<br>Raido : rune du voyage, à graver sur les tibias, qui augmente l'INIT de 1.<br>Hagalaz : rune de la grêle, à graver sur un arc, qui permet de tirer sans avoir de flèches."
+    // nom: "Graveur de runes",
+    // desc: "Le forgeron peut graver des runes mystiques sur la Norlithe afin de leur octroyer des capacités particulières.<br>Thurisaz : rune de la sauvagerie, à graver sur une arme, qui augmente ses DEG de 1.<br>Ansuz : rune de la connaissance, à graver sur le crâne, qui augmente l'ES de 1.<br>Raido : rune du voyage, à graver sur les tibias, qui augmente l'INIT de 1.<br>Hagalaz : rune de la grêle, à graver sur un arc, qui permet de tirer sans avoir de flèches."
+    nom: "Forge des Griffes et des Ecailles",
+    desc: "Le forgeron considère les griffes et les écailles des créatures touchées par la norlithe comme des armes et des armures forgées et peut les façonner."
   },
 
   // Les 3 branches avec leurs 3 compétences chacune
@@ -29,15 +31,15 @@ const CLASS_DATA = {
       competences: [
         {
           nom: "Brise-armure 1",
-          desc: "Sur un jet de PU, abaisse définitivement l’ARM adverse de 1 (2 si critique)."
+          desc: "Lorsque le forgeron effectue un CRIT au combat, il applique l'effet bris d'armure à sa cible, ce qui réduit son ARM de 1."
         },
         {
           nom: "Brise-armure 2",
-          desc: "Le seuil de CRIT baisse de 1 contre toutes les cibles portant une armure."
+          desc: "Le seuil de CRIT des cibles affectées par bris d'armure est abaissé de 1."
         },
         {
           nom: "Brise-armure 3",
-          desc: "Les attaques du forgeron ignorent l'armure."
+          desc: "Lorsque l'armure de la cible est déjà brisée, les CRIT du forgeron infligent un l'effet faiblesse. Le prochain coup réussi à la cible est automatiquement un CRIT."
         }
       ]
     },
@@ -54,7 +56,7 @@ const CLASS_DATA = {
         },
         {
           nom: "Affûter 3",
-          desc: "Sur un jet de FI, réduit le malus d’INIT d’une armure forgée de 1 (2 si critique). Non cumulable."
+          desc: "Améliore grandement les qualités d'une arme. Celle-ci obtient la description suivante : une fois par combat, une attaque normale devient un CRIT."
         }
       ]
     },
@@ -67,11 +69,11 @@ const CLASS_DATA = {
         },
         {
           nom: "Forgeron de guerre 2",
-          desc: "Le forgeron peut se battre en réalisant des bottes, qui utilisent sa FI plutôt que sa PU lors des jets de combat."
+          desc: "Le forgeron peut réaliséer des enchaînements. Lorsqu'il réussit une attaque de PU, il obtient une attaque gratuite de FI ce tour."
         },
         {
           nom: "Forgeron de guerre 3",
-          desc: "+1 aux jets de FI en combat."
+          desc: "Si l'action gratuite du forgeron est un CRIT, il obtient une attaque gratuite de PU ce tour."
         }
       ]
     }
