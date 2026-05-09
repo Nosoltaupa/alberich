d73b1b27609444622694e9d42af81055fc82c17c
Nosoltaupa <leonard.lefrere@proton.me>
2026-05-09 11:51:55 +0200

    Update classe.js

--- forgeron/classe.js
+++ forgeron/classe.js
@@ -17,7 +17,7 @@ const CLASS_DATA = {
     // nom: "Graveur de runes",
     // desc: "Le forgeron peut graver des runes mystiques sur la Norlithe afin de leur octroyer des capacités particulières.<br>Thurisaz : rune de la sauvagerie, à graver sur une arme, qui augmente ses DEG de 1.<br>Ansuz : rune de la connaissance, à graver sur le crâne, qui augmente l'ES de 1.<br>Raido : rune du voyage, à graver sur les tibias, qui augmente l'INIT de 1.<br>Hagalaz : rune de la grêle, à graver sur un arc, qui permet de tirer sans avoir de flèches."
     nom: "Forge des Griffes et des Ecailles",
-    desc: "Le forgeron considère les griffes et les écailles des créatures touchées par la norlithe comme des armes et des armures forgées et peut les façonner."
+    desc: "Le forgeron considère les griffes et les écailles des créatures touchées par la norlithe comme des armes et des armures forgées et peut les façonner à sa guise, que ce soit en récupérant ces matériaux sur des créatures vaincues pour forger de l'équipement ou pour modifier les propriétés naturelles des créatures touchées par la norlithe vivantes."
   },
 
   // Les 3 branches avec leurs 3 compétences chacune
@@ -39,7 +39,7 @@ const CLASS_DATA = {
         },
         {
           nom: "Brise-armure 3",
-          desc: "Lorsque l'armure de la cible est déjà brisée, les CRIT du forgeron infligent un l'effet faiblesse. Le prochain coup réussi à la cible est automatiquement un CRIT."
+          desc: "Lorsque l'armure de la cible est déjà brisée, les CRIT du forgeron infligent l'effet faiblesse. La prochaine attaque réussie contre cette cible est automatiquement un CRIT."
         }
       ]
     },
@@ -68,12 +68,12 @@ const CLASS_DATA = {
           desc: "Les DEG sont augmentés de 1 en maniant une arme forgée."
         },
         {
-          nom: "Forgeron de guerre 2",
-          desc: "Le forgeron peut réaliséer des enchaînements. Lorsqu'il réussit une attaque de PU, il obtient une attaque gratuite de FI ce tour."
+          nom: "Botte",
+          desc: "Lorsque le forgeron réussit une attaque de PU, il obtient une attaque gratuite de FI ce tour."
         },
         {
-          nom: "Forgeron de guerre 3",
-          desc: "Si l'action gratuite du forgeron est un CRIT, il obtient une attaque gratuite de PU ce tour."
+          nom: "Enchaînement",
+          desc: "Si la botte du forgeron est un CRIT, il obtient une attaque gratuite de PU ce tour. Cette action peut déclencher une botte."
         }
       ]
     }
